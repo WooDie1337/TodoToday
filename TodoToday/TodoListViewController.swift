31ff22577b8dcf45b24660d2bbb4a4f3e6b49224
Daniel Krupenin <woodie@macbook-pro-daniel.local>
2018-02-14 10:59:35 -0800

    Todo list complete with datasource and delegate methods.

---
+++ TodoToday/TodoListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TodoToday
+//
+//  Created by Daniel Krupenin on 14.02.2018.
+//  Copyright © 2018 Daniel Krupenin. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController{
+    let itemArray : [String] = ["Find Mike","Buy Eggos","Destroy Demogorgon"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+

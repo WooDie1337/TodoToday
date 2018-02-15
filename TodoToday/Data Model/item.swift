eed2818eea84bd8b2527e3a0cca5b1b5bfba747d
Daniel Krupenin <woodie@macbook-pro-daniel.local>
2018-02-15 10:30:50 -0800

    persisting data using Codable protocol and plist.

---
+++ TodoToday/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  TodoToday
+//
+//  Created by Daniel Krupenin on 14.02.2018.
+//  Copyright © 2018 Daniel Krupenin. All rights reserved.
+//
+
+import Foundation
+
+class Item : Codable {
+    var title : String = ""
+    var done : Bool = false
+}
